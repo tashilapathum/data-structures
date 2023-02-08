3a15bbb7b2a7fb0996f94311bef6a899d4dc062a
Tashila Pathum <tashila.pathum@gmail.com>
2023-02-08 14:26:00 +0530

    Added bubble and insertion sort

--- src/algorithms/sorting/Sort.java
+++ src/algorithms/sorting/Sort.java
@@ -5,7 +5,10 @@ import utils.Logger;
 public class Sort {
 
     public static void main(String[] args) {
-        selectionSort(new int[] {32,32,543,534,5,43,6,35,43,45,3});
+        int[] arr = new int[]{32, 14, 23, 45, 5, 43, 6, 45, 7, 5, 8, 769, 87};
+        selectionSort(arr);
+        bubbleSort(arr);
+        insertionSort(arr);
     }
 
     public static void selectionSort(int[] array) {
@@ -14,7 +17,7 @@ public class Sort {
         //loop all value except the last one
         for (int i = 0; i < array.length - 1; i++) {
             minValueIndex = i; //assume min value to be the currently iterating one
-            for (int j = i+1; j < array.length; j++)
+            for (int j = i + 1; j < array.length; j++)
                 if (array[j] < array[minValueIndex])
                     minValueIndex = j; //reassign min value if it's smaller than the assumed one
 
@@ -22,6 +25,41 @@ public class Sort {
             temp = array[i];
             array[i] = array[minValueIndex];
             array[minValueIndex] = temp;
+        }
+
+        new Logger(array).print();
+    }
+
+    public static void bubbleSort(int[] array) {
+        for (int i = array.length - 1; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    //swap
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        new Logger(array).print();
+    }
+
+    public static void insertionSort(int[] array) {
+        int key, space;
+        for (int i = 1; i < array.length; i++) {
+            key = array[i];
+            space = i;
+
+            for (int j = space - 1; j >= 0; j--) {
+                if (array[j] > key) {
+                    array[j + 1] = array[j];
+                    space = j;
+                    continue;
+                }
+                break;
+            }
+            array[space] = key;
         }
 
         new Logger(array).print();
